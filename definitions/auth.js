2e90798d39bd7d356a1dbac472a20e1f2bc66204
Peter Širka <petersirka@gmail.com>
2016-06-23 22:37:33 +0200

    New changes.

--- definitions/auth.js
+++ definitions/auth.js
@@ -1,9 +1,10 @@
 F.onAuthorize = function(req, res, flags, callback ) {
 
-	if (F.config['openplatform.testmode'])
+	if (F.config['openplatform.testmode']) {
+		// OPENPLATFORM.testuser([roles], [settings])
 		return callback(true, OPENPLATFORM.testuser());
+	}
 
 	var user = OPENPLATFORM.session(req);
 	callback(user ? true : false, user);
 };
-
