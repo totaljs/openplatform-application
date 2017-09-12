8c715e8bc6e3073c1e98708bedade996e70ddb64
Peter Širka <petersirka@gmail.com>
2017-09-12 15:29:15 +0200

    Add a support for server-side implementation.

--- controllers/default.js
+++ controllers/default.js
@@ -1,3 +1,18 @@
 exports.install = function() {
 	ROUTE('/');
+
+	// Server-Side verification
+	ROUTE('/verify/', json_verify);
 };
+
+function json_verify() {
+	// Server-Side verification for OpenPlatform meta-data
+	var self = this;
+	var builder = new RESTBuilder(self.query.url);
+	builder.exec(function(err, response) {
+		if (err)
+			self.invalid().push(err);
+		else
+			self.json(response);
+	});
+}
