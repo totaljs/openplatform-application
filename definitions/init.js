7d8ff3e65aa461a5fbd9b111471037abe3280949
Peter Širka <petersirka@gmail.com>
2023-11-22 10:52:25 +0100

    Updated UI components.

--- definitions/init.js
+++ definitions/init.js
@@ -20,7 +20,7 @@ CONF.op_icon = 'ti ti-rocket';
 LOADCONFIG(db.config);
 
 // UI components
-COMPONENTATOR('ui', 'exec,locale,aselected,page,viewbox,input,importer,box,validate,loading,selected,intranetcss,notify,message,errorhandler,empty,menu,ready,fileuploader,colorpicker,approve,icons,directory', true);
+COMPONENTATOR('ui', 'exec,locale,aselected,page,viewbox,input,importer,box,validate,loading,selected,intranetcss,notify,message,errorhandler,empty,menu,ready,fileuploader,colorpicker,approve,icons,directory,uibuilder,uistudio', true);
 
 // Permissions
 ON('ready', function() {
